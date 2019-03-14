23955e9d6a89eee92b14e15a81ae902c46b1280f
Your Name <github email address>
2019-03-14 16:20:59 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    spacePeople: []
+  }
+
+  componentDidMount(){
+    console.log("mounted")
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json())
+    .then(({people}) => this.setState({ spacePeople: people }))
+  }
+
+
+  render() {
+    return (
+      <div>
+        {this.state.spacePeople.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+      </div>
+    )
+  }
+}
+export default App
